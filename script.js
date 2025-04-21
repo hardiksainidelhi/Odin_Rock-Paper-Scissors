d60129179ed2abfb6ad829fd606304e9e677ff4a
hardiksainidelhi <hardiksainidelhi@gmail.com>
2025-04-21 03:55:49 +0000

    Add Game function for 5 rounds

--- script.js
+++ script.js
@@ -1,10 +1,79 @@
-// Initialize user and machine score
 
-let userScore = 0;
-let machineScore = 0;
 
-// Game Round
 
+function playGame(){
+    // Initialize user and machine score
+    let userScore = 0;
+    let machineScore = 0;
+
+    const rounds = 5;
+    for (let i = 0;i<rounds;i++){
+        let userChoice = getHumanChoice()
+        let machineChoice = generateComputerChoice()
+        message = playRound(userChoice,machineChoice)
+        console.log(
+            `Round ${i+1}!\n
+            your choice: ${userChoice}\n
+            machine choice: ${machineChoice}\n
+            result: ${message}\n
+            score: you:${userScore} vs machine${machineScore}`
+        )
+    }
+
+    (machineScore > userScore) ? alert('You Lose!') : alert('You Win!');
+    
+    // Game Round
+    function playRound(userChoice,machineChoice){
+        let message;
+        switch (userChoice){
+            case 'ROCK':
+                if (machineChoice === 'PAPER'){
+                    machineScore ++
+                    message = 'You lose this round ❌'
+                }
+                else if (machineChoice == 'SCISSORS'){
+                    userScore ++;
+                    message = 'You win this round ✅ '
+                }
+                else {
+                    message = 'Tie'
+                }
+                
+                break;
+            case 'PAPER':
+                if (machineChoice === 'SCISSOR'){
+                    machineScore ++
+                    message = 'You lose this round'
+                }
+                else if (machineChoice == 'ROCK'){
+                    userScore ++;
+                    message = 'You win this round ✅ '
+                }
+                else {
+                    message = 'Tie'
+                }
+                break;
+            case 'SCISSOR':
+                if (machineChoice === 'ROCK'){
+                    machineScore ++
+                    message = 'You lose this round ❌'
+                }
+                else if (machineChoice == 'PAPER'){
+                    userScore ++;
+                    message = 'You win this round ✅ '
+                }
+                else {
+                    message = 'Tie'
+                }
+                break;
+        }
+        return message
+    }
+
+    
+}
+
+playGame()
 
 // Get Human choice from prompt
 function getHumanChoice() {
