cb53d302df9a01095dd2660188392f9e7442f0d4
hardiksainidelhi <hardiksainidelhi@gmail.com>
2025-04-26 06:21:14 +0000

    Migrate console play to UI

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
 
 
 
-function playGame(){
+function playGame() {
     // Initialize user and machine score
     let userScore = 0;
     let machineScore = 0;
 
-    const rounds = 5;
-    for (let i = 0;i<rounds;i++){
-        let userChoice = getHumanChoice()
-        let machineChoice = generateComputerChoice()
-        message = playRound(userChoice,machineChoice)
-        alert(
-            `Round ${i+1}!\n
-            your choice: ${userChoice}\n
-            machine choice: ${machineChoice}\n
-            result: ${message}\n
-            score: you:${userScore} vs machine: ${machineScore}`
-        )
-    }
+    const playerChoiceBtns = document.querySelectorAll('button')
+    const resultDiv = document.querySelector('#result')
 
-    (machineScore > userScore) ? alert('You Lose!') : alert('You Win!');
-    
+    playerChoiceBtns.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            const playerChoice = event.target.textContent.toUpperCase()
+            const machineChoice = generateComputerChoice()
+            
+            if (userScore < 5){
+                const message = playRound(playerChoice, machineChoice)
+
+                resultDiv.innerText = `your choice: ${playerChoice} and machine choice: ${machineChoice} \n
+                                        ${message} \n
+                                        score( you:${userScore} vs machine: ${machineScore} )`
+
+            }
+            else {
+                resultDiv.innerText = (userScore>machineScore) ?
+                 'You win' : (userScore = machineChoice) ? 
+                 'Tie': 'You lose'
+            }
+        })
+    })
+
     // Game Round
-    function playRound(userChoice,machineChoice){
+    function playRound(userChoice, machineChoice) {
         let message;
-        switch (userChoice){
+        switch (userChoice) {
             case 'ROCK':
-                if (machineChoice === 'PAPER'){
-                    machineScore ++
+                if (machineChoice === 'PAPER') {
+                    machineScore++
                     message = 'You lose this round ❌'
                 }
-                else if (machineChoice == 'SCISSORS'){
-                    userScore ++;
+                else if (machineChoice == 'SCISSOR') {
+                    userScore++;
                     message = 'You win this round ✅ '
                 }
                 else {
                     message = 'Tie'
                 }
-                
+
                 break;
             case 'PAPER':
-                if (machineChoice === 'SCISSOR'){
-                    machineScore ++
-                    message = 'You lose this round'
+                if (machineChoice === 'SCISSOR') {
+                    machineScore++
+                    message = 'You lose this round ❌'
                 }
-                else if (machineChoice == 'ROCK'){
-                    userScore ++;
+                else if (machineChoice == 'ROCK') {
+                    userScore++;
                     message = 'You win this round ✅ '
                 }
                 else {
@@ -54,12 +62,12 @@ function playGame(){
                 }
                 break;
             case 'SCISSOR':
-                if (machineChoice === 'ROCK'){
-                    machineScore ++
+                if (machineChoice === 'ROCK') {
+                    machineScore++
                     message = 'You lose this round ❌'
                 }
-                else if (machineChoice == 'PAPER'){
-                    userScore ++;
+                else if (machineChoice == 'PAPER') {
+                    userScore++;
                     message = 'You win this round ✅ '
                 }
                 else {
@@ -70,7 +78,7 @@ function playGame(){
         return message
     }
 
-    
+
 }
 
 playGame()
@@ -81,11 +89,11 @@ function getHumanChoice() {
     choice = prompt("Enter Choice: ")
     return choice.toUpperCase()
 
-    }
-    
+}
+
 // Generate machine choice
-function generateComputerChoice(){
-    let c = Math.floor(Math.random()*3) + 1
+function generateComputerChoice() {
+    let c = Math.floor(Math.random() * 3) + 1
     let choice;
 
     switch (c) {
@@ -102,3 +110,4 @@ function generateComputerChoice(){
 
     return choice;
 }
+
